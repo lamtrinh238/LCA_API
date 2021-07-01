2ae46174f9cdd01a1077f2943f17060e69c0330f
LamTrinh <tamamigo01@gmail.com>
2021-07-02 00:11:29 +0700

    test commit

---
+++ lca.services/Models/RegisterModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LCA.Service.Models
+{
+    public class RegisterModel
+    {
+        [Required(ErrorMessage = "User Name is required")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; }
+    }
+}
